48ef33b93a9d03436434aaf8d19914165de75f63
Rocky <rrsalerno21@gmail.com>
2020-04-22 20:05:29 -0700

    added body data titles, function constructor for PlayerScore

--- assets/script.js
+++ assets/script.js
@@ -45,173 +45,193 @@ var storage = {
     score: 0,
     answersCorrect: 0,
     answersIncorrect: 0,
-    interval: true //Creating this variable here to reference for our timer intervals
+    interval: true, //Creating this variable here to reference for our timer intervals
+    player_scores: []
 }
 
+// Function constructor to record player scores
+function PlayerScore(initials, score) {
+    this.initials = initials;
+    this.score = score;
+}
+
+
+
 $(document).ready(function() {
-    // Functions
-    function startGameTimer() {
-        storage.interval = setInterval(function() {
-            $('#timer-display').css({'color': 'inherit', 'font-size': '18px'});
-            storage.gameTimer--;
-            $('#timer-display').html(storage.gameTimer);
-
-
-            if (storage.gameTimer <= 10 ) {
-                $('#timer-display').css({'color': 'red', 'font-size': '30px'});
-            }
-
-            if (storage.gameTimer <= 0) {
-                $('#game-over-heading').html(`Time's Up!`);
-                displaySubmitScreen();
-            }
-        }, 1000);
-    };
-
-    // Render next question function
-    function renderQuestion() {
-        // Generate the next random question by picking a random number
-        var ranNum = Math.floor(Math.random() * storage.questions.length);
-
-        // Check to see if that random number has already been generated
-        while (storage.randomCheckArray.includes(ranNum)) {
-            ranNum = (ranNum + 1) % storage.questions.length;
-        }
-        
-        // Push that random index number into our storage array
-        storage.randomCheckArray.push(ranNum);
-
-        // Set our next question
-        var nextQuestion = storage.questions[ranNum];
-        storage.curQuestion = nextQuestion;
-
-        // Display the next random question
-        var questionBox = document.getElementById('question-div-box');
-
-        questionBox.innerHTML = (`<h2 class="question-title">${nextQuestion.question}</h2><div id="answer-list"><button class="ans-btn" id="answer_0" value="1">1.)  ${nextQuestion.answer_0}</button><button class="ans-btn" id="answer_1">2.)  ${nextQuestion.answer_1}</button><button class="ans-btn" id="answer_2">3.)  ${nextQuestion.answer_2}</button><button class="ans-btn" id="answer_3">4.)  ${nextQuestion.answer_3}</button></div>`);
-    };
-
-    // Stop game and submit screen function
-    function displaySubmitScreen() {
-        //stop the timer and hide the timer
-        clearInterval(storage.interval);
-        $('#timer-box').toggle('hide');
-
-        //update final-score
-        $('#final-score').html(`Your Final Score: ${storage.score}`);
-
-        // display the submit score screen
-        $('#time-up-box').toggle('hide');
-        $('#question-div-box').toggle('hide');
-    }
-
-    // function to initiate the game
-    function startGame() {
-        // Hide the start screen
-        $('#intro-div').toggle('hide');
-        
-        renderQuestion();
-
-        // Start a 60 second timer
-        startGameTimer();
-
-        // Display timer
-        $('#timer-box').toggle('hide');
-
-
-        // Display the #question-div-box
-        $('#question-div-box').toggle('hide');
-    }
-
-    // add answer response function
-    function addResponse(result) {
-        var responseTimerInterval, responseTimer = 1;
-        if (result==="Correct") {
-            // display UI response for correct for 3 seconds
-            $('#answer-response-box').html('<hr><p>Correct!<p>');
-            responseTimerInterval = setInterval(function(){
-                responseTimer--;
-                if (responseTimer === 0) {
-                    $('#answer-response-box').html('');
-                    clearInterval(responseTimerInterval);
+    if ($('body').data('title') === 'quiz-page') {
+        // Functions
+        function startGameTimer() {
+            storage.interval = setInterval(function() {
+                $('#timer-display').css({'color': 'inherit', 'font-size': '18px'});
+                storage.gameTimer--;
+                $('#timer-display').html(storage.gameTimer);
+    
+    
+                if (storage.gameTimer <= 10 ) {
+                    $('#timer-display').css({'color': 'red', 'font-size': '30px'});
                 }
-            }, 1000)
-        } else {
-            // display UI response for incorrect for 3 seconds
-            console.log('wrong');
-            $('#answer-response-box').append('<hr><p>Incorrect!<p>');
-            responseTimerInterval = setInterval(function(){
-                responseTimer--;
-                if (responseTimer === 0) {
-                    $('#answer-response-box').html('');
-                    clearInterval(responseTimerInterval);
+    
+                if (storage.gameTimer <= 0) {
+                    $('#game-over-heading').html(`Time's Up!`);
+                    displaySubmitScreen();
                 }
             }, 1000);
-
-            // stop current timer, subtract gameTimer by 10 sec and immediately display the new time
-            clearInterval(storage.interval)
-            storage.gameTimer -= 10;
-
-            if (storage.gameTimer <= 0) {
-                displaySubmitScreen();
-                $('#game-over-heading').html(`Time's Up!`);
-                return
-            } else {
-                $('#timer-display').html(storage.gameTimer);
-
-                // start the timer again with the new time
-                startGameTimer();
-            }
-        }
-    }
-
-
-    // EVENT LISTENERS
-    //=========================================================
-
-    // Start Button Event Listener
-    $('#start-button').on('click', startGame);
-    $('#retry-button').on('click', function() {
-        console.log('please dont break me');
-        location.reload(true);
-    });
+        };
     
-
-
-
-    // Question answer buttons Event Listener.  Cannot use jQuery here because we are dynamically generating this HTML
-    document.getElementById('question-div-box').addEventListener('click', function(event) {
-
-        // make sure the button is clicked
-        var buttonClicked = event.target;
-        
-        if (buttonClicked.matches("button")) {
-
-            // Check to see if the answer was right or wrong
-            if (storage.curQuestion.correct_answer === buttonClicked.id) {
+        // Render next question function
+        function renderQuestion() {
+            // Generate the next random question by picking a random number
+            var ranNum = Math.floor(Math.random() * storage.questions.length);
+    
+            // Check to see if that random number has already been generated
+            while (storage.randomCheckArray.includes(ranNum)) {
+                ranNum = (ranNum + 1) % storage.questions.length;
+            }
+            
+            // Push that random index number into our storage array
+            storage.randomCheckArray.push(ranNum);
+    
+            // Set our next question
+            var nextQuestion = storage.questions[ranNum];
+            storage.curQuestion = nextQuestion;
+    
+            // Display the next random question
+            var questionBox = document.getElementById('question-div-box');
+    
+            questionBox.innerHTML = (`<h2 class="question-title">${nextQuestion.question}</h2><div id="answer-list"><button class="ans-btn" id="answer_0" value="1">1.)  ${nextQuestion.answer_0}</button><button class="ans-btn" id="answer_1">2.)  ${nextQuestion.answer_1}</button><button class="ans-btn" id="answer_2">3.)  ${nextQuestion.answer_2}</button><button class="ans-btn" id="answer_3">4.)  ${nextQuestion.answer_3}</button></div>`);
+        };
+    
+        // Stop game and submit screen function
+        function displaySubmitScreen() {
+            //stop the timer and hide the timer
+            clearInterval(storage.interval);
+            $('#timer-box').toggle('hide');
+    
+            //update final-score
+            $('#final-score').html(`Your Final Score: ${storage.score}`);
+    
+            // display the submit score screen
+            $('#time-up-box').toggle('hide');
+            $('#question-div-box').toggle('hide');
+        }
+    
+        // function to initiate the game
+        function startGame() {
+            // Hide the start screen
+            $('#intro-div').toggle('hide');
+            
+            renderQuestion();
+    
+            // Start a 60 second timer
+            startGameTimer();
+    
+            // Display timer
+            $('#timer-box').toggle('hide');
+    
+    
+            // Display the #question-div-box
+            $('#question-div-box').toggle('hide');
+        }
+    
+        // add answer response function
+        function addResponse(result) {
+            var responseTimerInterval, responseTimer = 1;
+            if (result==="Correct") {
                 // display UI response for correct for 3 seconds
-                addResponse("Correct");
-                storage.score += 1;
-                storage.answersCorrect += 1;
+                $('#answer-response-box').html('<hr><p>Correct!<p>');
+                responseTimerInterval = setInterval(function(){
+                    responseTimer--;
+                    if (responseTimer === 0) {
+                        $('#answer-response-box').html('');
+                        clearInterval(responseTimerInterval);
+                    }
+                }, 1000)
             } else {
                 // display UI response for incorrect for 3 seconds
-                addResponse("Incorrect");
-                storage.answersIncorrect += 1;
+                console.log('wrong');
+                $('#answer-response-box').append('<hr><p>Incorrect!<p>');
+                responseTimerInterval = setInterval(function(){
+                    responseTimer--;
+                    if (responseTimer === 0) {
+                        $('#answer-response-box').html('');
+                        clearInterval(responseTimerInterval);
+                    }
+                }, 1000);
+    
+                // stop current timer, subtract gameTimer by 10 sec and immediately display the new time
+                clearInterval(storage.interval)
+                storage.gameTimer -= 10;
+    
+                if (storage.gameTimer <= 0) {
+                    displaySubmitScreen();
+                    $('#game-over-heading').html(`Time's Up!`);
+                    return
+                } else {
+                    $('#timer-display').html(storage.gameTimer);
+    
+                    // start the timer again with the new time
+                    startGameTimer();
+                }
             }
-        };
-
-        // if all questions have been answered, display Submit Screen
-        if (storage.randomCheckArray.length === storage.questions.length){
-            displaySubmitScreen();
-            $('#game-over-heading').html('All Questions Answered!');
-            return;
         }
+    
+    
+        // EVENT LISTENERS
+        //=========================================================
+    
+        // Start Button Event Listener
+        $('#start-button').on('click', startGame);
+        $('#retry-button').on('click', function() {
+            console.log('please dont break me');
+            location.reload(true);
+        });
+        
+    
+    
+    
+        // Question answer buttons Event Listener.  Cannot use jQuery here because we are dynamically generating this HTML
+        document.getElementById('question-div-box').addEventListener('click', function(event) {
+    
+            // make sure the button is clicked
+            var buttonClicked = event.target;
+            
+            if (buttonClicked.matches("button")) {
+    
+                // Check to see if the answer was right or wrong
+                if (storage.curQuestion.correct_answer === buttonClicked.id) {
+                    // display UI response for correct for 3 seconds
+                    addResponse("Correct");
+                    storage.score += 1;
+                    storage.answersCorrect += 1;
+                } else {
+                    // display UI response for incorrect for 3 seconds
+                    addResponse("Incorrect");
+                    storage.answersIncorrect += 1;
+                }
+            };
+    
+            // if all questions have been answered, display Submit Screen
+            if (storage.randomCheckArray.length === storage.questions.length){
+                displaySubmitScreen();
+                $('#game-over-heading').html('All Questions Answered!');
+                return;
+            }
+    
+            // Generate the next random question by picking a random number
+            renderQuestion();
+        });
 
-        // Generate the next random question by picking a random number
-        renderQuestion();
-    });
+    }
 
+    if ($('body').data('title') === 'high-scores-page') {
+        console.log(storage);
+    }
 });
+
+
+
+
+
 
 
 
